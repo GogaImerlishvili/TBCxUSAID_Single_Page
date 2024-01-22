afd9aa95ceb0bf83ecf3a15a3c760a80806d9544
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-01-22 15:17:00 +0400

    create func for burger menu

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header")
+
+btnNavEl.addEventListener("click", function() {
+    headerEl.classList.toggle("nav-open")
+})
